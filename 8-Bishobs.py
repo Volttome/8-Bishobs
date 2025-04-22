700d59deffd0f4c2795fe852195f1c1a3a5425ae
Volttome <97765530+Volttome@users.noreply.github.com>
2025-04-22 09:28:54 -1200

    8-Queens.py

--- 8-Bishobs.py
+++ 8-Bishobs.py
@@ -1,49 +1,48 @@
 import time , os
-os.system('cls') ; print("\n"*2) ; n = int(input("Enter the size of the board : ")) ; print("\n"*2)
-bord = [[' .' for _ in range(n)] for _ in range(n)]
+os.system('cls'); print("\n"*2)
+n = int(input("Enter the size of the board : ")); print("\n"*2)
+board = [[' .' for _ in range(n)] for _ in range(n)]
 
-def safe(row, col):   #check row
+
+def safe(row, col):  # check row
     for j in range(col):
-        if bord[row][j] == ' ∎':
+        if board[row][j] == ' ∎':
             return False
-            
-    for i in range(1, col + 1): #check original diyagnal
-        r = row - i 
-        c = col - i
+
+    for i in range(1, col + 1):  # check original diameters
+        r = row - i; c = col - i
         if r >= 0 and c >= 0:
-            if bord[r][c] == ' ∎':
-                return False
-                
-    for i in range(1, col + 1): #check not original diyagnal
-        r = row + i
-        c = col - i
-        if r < n and c >= 0:
-            if bord[r][c] == ' ∎':
+            if board[r][c] == ' ∎':
                 return False
 
+    for i in range(1, col + 1):  # check not original diameters
+        r = row + i; c = col - i
+        if r < n and c >= 0:
+            if board[r][c] == ' ∎':
+                return False
     return True
 
-def show(): #showing the bord in reall time
-    os.system('cls')
-    print("\n"*4)
-    for r in bord:
+
+def show():  # showing the  board in reall time
+    os.system('cls'); print("\n"*4)
+    for r in board:
         for j in r:
             print(j, end='      ')
         print("\n"*2)
-    time.sleep(0.8)
+    time.sleep(0.5)
+
 
 def solve(col):
     if col == n:
         return True
-
-    for row in range(n):  #checking eche col for best row 
-        bord[row][col] = ' ?' ; show() ; bord[row][col] = ' .' #show tring
+        
+    for row in range(n):  # checking eche col for best row
+        board[row][col] = ' ?'; show();  board[row][col] = ' .'  # show tring
         if safe(row, col):
-            bord[row][col] = ' ∎'
-            show()
+            board[row][col] = ' ∎' ; show()
             if solve(col + 1):  #going for the next col
                 return True
-            bord[row][col] = ' .'  # Deleting the Bishobs for back tracking
-            show()
+            board[row][col] = ' .' ; show() # Deleting the Bishobs for back tracking
     return False
+    
 solve(0) #start this shitt show whit col 0
